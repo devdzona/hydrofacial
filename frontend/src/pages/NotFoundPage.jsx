0d14f6983ee239bbfb710e68ea2a3fcd4f4ebbc0
Nikola Ostojic <dzonaberlin@gmail.com>
2024-11-11 22:36:56 +0100

    upadte 404 page

--- frontend/src/pages/NotFoundPage.jsx
+++ frontend/src/pages/NotFoundPage.jsx
@@ -1,11 +1,15 @@
 /* eslint-disable no-unused-vars */
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const NotFoundPage = () => {
   return (
     <div className="not-found-page">
-      <h2>404 - Page Not Found</h2>
-      <p>The page you are looking for does not exist.</p>
+      <h1 className="title">404</h1>
+      <p className="message">Oops! The page you’re looking for doesn’t exist.</p>
+      <Link to="/" className="button">
+        Go Back Home
+      </Link>
     </div>
   );
 };
